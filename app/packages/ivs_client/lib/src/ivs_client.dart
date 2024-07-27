0a48abfde1e92795410b2d52d5f99cba0c6528bf
Parker Stromberg <parker.stromberg@gmail.com>
2024-07-27 12:21:14 -0600

    added ivs client

---
+++ app/packages/ivs_client/lib/src/ivs_client.dart
@@ -0,0 +1,7 @@
+/// {@template ivs_client}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class IvsClient {
+  /// {@macro ivs_client}
+  const IvsClient();
+}
---
+++ app/packages/ivs_client/test/src/ivs_client_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ivs_client/ivs_client.dart';
+
+void main() {
+  group('IvsClient', () {
+    test('can be instantiated', () {
+      expect(IvsClient(), isNotNull);
+    });
+  });
+}
